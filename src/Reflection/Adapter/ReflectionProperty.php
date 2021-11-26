86e51b1dcb076f2ed7a2d8e0d0d7f59cb480ebea
Ondrej Mirtes <ondrej@mirtes.cz>
2021-11-26 13:40:03 +0100

    Adapter ReflectionMethod and ReflectionProperty - get access to the underlying BetterReflection object

--- src/Reflection/Adapter/ReflectionProperty.php
+++ src/Reflection/Adapter/ReflectionProperty.php
@@ -353,4 +353,9 @@ final class ReflectionProperty extends CoreReflectionProperty
 
         throw new OutOfBoundsException(sprintf('Property %s::$%s does not exist.', self::class, $name));
     }
+
+    public function getBetterReflection(): BetterReflectionProperty
+    {
+        return $this->betterReflectionProperty;
+    }
 }
