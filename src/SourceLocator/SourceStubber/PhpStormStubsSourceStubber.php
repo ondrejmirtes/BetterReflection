4da4dd61721e2d83d9699a866fb03bd4fafde619
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-01 17:51:30 +0100

    Make it easier for downgrading

--- src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
+++ src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
@@ -714,7 +714,7 @@ final class PhpStormStubsSourceStubber implements SourceStubber
         }
 
         foreach ($deprecatedAttribute->args as $attributeArg) {
-            if ($attributeArg->name?->toString() === 'since') {
+            if ($attributeArg->name !== null && $attributeArg->name->toString() === 'since') {
                 assert($attributeArg->value instanceof Node\Scalar\String_);
 
                 return $this->parsePhpVersion($attributeArg->value->value) <= $this->phpVersion;
@@ -763,7 +763,7 @@ final class PhpStormStubsSourceStubber implements SourceStubber
                     $fromVersion = $this->parsePhpVersion($attributeArg->value->value);
                 }
 
-                if ($attributeArg->name?->toString() !== 'to') {
+                if ($attributeArg->name !== null && $attributeArg->name->toString() !== 'to') {
                     continue;
                 }
 
