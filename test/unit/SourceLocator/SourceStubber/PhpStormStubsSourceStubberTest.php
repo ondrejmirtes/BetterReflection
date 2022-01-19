fb8c8e64f324c95c90276ea09c074b520f81a5a9
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-19 14:51:57 +0100

    PhpStormStubsSourceStubber - report class filename

--- test/unit/SourceLocator/SourceStubber/PhpStormStubsSourceStubberTest.php
+++ test/unit/SourceLocator/SourceStubber/PhpStormStubsSourceStubberTest.php
@@ -1310,4 +1310,10 @@ class PhpStormStubsSourceStubberTest extends TestCase
 
         self::assertNull($stub);
     }
+
+    public function testFilename(): void
+    {
+        $reflection = $this->reflector->reflectClass('XMLReader');
+        $this->assertSame(realpath(__DIR__ . '/../../../../vendor/jetbrains/phpstorm-stubs/xmlreader/xmlreader.php'), realpath($reflection->getFileName()));
+    }
 }
