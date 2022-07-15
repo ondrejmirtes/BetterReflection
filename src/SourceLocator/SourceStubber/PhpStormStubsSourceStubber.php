7a978704c58ec27051301b1d59a8a34210b5bc26
Ondrej Mirtes <ondrej@mirtes.cz>
2022-07-15 11:01:01 +0200

    Allow SensitiveParameter on any PHP version

--- src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
+++ src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
@@ -401,6 +401,7 @@ final class PhpStormStubsSourceStubber implements SourceStubber
                     $className !== 'Attribute'
                     && $className !== 'ReturnTypeWillChange'
                     && $className !== 'AllowDynamicProperties'
+                    && $className !== 'SensitiveParameter'
                     && ! $this->isSupportedInPhpVersion($classNode)
                 ) {
                     continue;
