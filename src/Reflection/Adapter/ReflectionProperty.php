5200e492d91e0cda2be750e8111ea774bf1cb266
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-26 15:25:17 +0200

    Adapter - make underlying initializer expressions available

--- src/Reflection/Adapter/ReflectionProperty.php
+++ src/Reflection/Adapter/ReflectionProperty.php
@@ -6,6 +6,7 @@ namespace Roave\BetterReflection\Reflection\Adapter;
 
 use ArgumentCountError;
 use OutOfBoundsException;
+use PhpParser\Node\Expr;
 use PropertyHookType;
 use ReflectionException as CoreReflectionException;
 use ReflectionMethod as CoreReflectionMethod;
@@ -232,12 +233,26 @@ final class ReflectionProperty extends CoreReflectionProperty
     }
 
     /**
+     * @deprecated Use getDefaultValueExpression()
      * @return mixed
      */
     #[ReturnTypeWillChange]
     public function getDefaultValue()
     {
         return $this->betterReflectionProperty->getDefaultValue();
+    }
+
+    /**
+     * @deprecated Use getDefaultValueExpression()
+     */
+    public function getDefaultValueExpr(): Expr
+    {
+        return $this->betterReflectionProperty->getDefaultValueExpression();
+    }
+
+    public function getDefaultValueExpression(): Expr
+    {
+        return $this->betterReflectionProperty->getDefaultValueExpression();
     }
 
     /**
