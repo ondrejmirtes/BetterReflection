20258fab1ccaa26ef2bf6a9775b863d40790257f
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-26 15:25:17 +0200

    Adapter - make underlying initializer expressions available

--- src/Reflection/Adapter/ReflectionClass.php
+++ src/Reflection/Adapter/ReflectionClass.php
@@ -254,6 +254,8 @@ final class ReflectionClass extends CoreReflectionClass
     }
 
     /**
+     * @deprecated Use getReflectionConstants()
+     *
      * @param int-mask-of<ReflectionClassConstant::IS_*>|null $filter
      *
      * @return array<non-empty-string, mixed>
@@ -270,7 +272,8 @@ final class ReflectionClass extends CoreReflectionClass
     }
 
     /**
-     * {@inheritDoc}
+     * @deprecated Use getReflectionConstant()
+     * @return mixed
      */
     #[ReturnTypeWillChange]
     public function getConstant($name)
@@ -298,7 +301,7 @@ final class ReflectionClass extends CoreReflectionClass
     private function getConstantValue(BetterReflectionClassConstant|BetterReflectionEnumCase $betterConstantOrEnumCase): mixed
     {
         if ($betterConstantOrEnumCase instanceof BetterReflectionEnumCase) {
-            return constant(sprintf('%s::%s', $betterConstantOrEnumCase->getDeclaringClass()->getName(), $betterConstantOrEnumCase->getName()));
+            throw new Exception\NotImplemented('Not implemented');
         }
 
         return $betterConstantOrEnumCase->getValue();
--- src/Reflection/Adapter/ReflectionEnumBackedCase.php
+++ src/Reflection/Adapter/ReflectionEnumBackedCase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Roave\BetterReflection\Reflection\Adapter;
 
 use OutOfBoundsException;
+use PhpParser\Node\Expr;
 use ReflectionEnumBackedCase as CoreReflectionEnumBackedCase;
 use Roave\BetterReflection\Reflection\ReflectionAttribute as BetterReflectionAttribute;
 use Roave\BetterReflection\Reflection\ReflectionEnumCase as BetterReflectionEnumCase;
@@ -124,6 +125,9 @@ final class ReflectionEnumBackedCase extends CoreReflectionEnumBackedCase
         return new ReflectionEnum($this->betterReflectionEnumCase->getDeclaringEnum());
     }
 
+    /**
+     * @deprecated Use getValueExpression()
+     */
     public function getBackingValue(): int|string
     {
         return $this->betterReflectionEnumCase->getValue();
@@ -132,6 +136,19 @@ final class ReflectionEnumBackedCase extends CoreReflectionEnumBackedCase
     public function isDeprecated(): bool
     {
         return $this->betterReflectionEnumCase->isDeprecated();
+    }
+
+    /**
+     * @deprecated Use getValueExpression()
+     */
+    public function getValueExpr(): Expr
+    {
+        return $this->getValueExpression();
+    }
+
+    public function getValueExpression(): Expr
+    {
+        return $this->betterReflectionEnumCase->getValueExpression();
     }
 
     public function __get(string $name): mixed
