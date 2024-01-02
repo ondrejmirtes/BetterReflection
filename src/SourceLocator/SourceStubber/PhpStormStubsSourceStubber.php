5891da907b3f99ff0de182a766aacde078e7c9b2
Ondrej Mirtes <ondrej@mirtes.cz>
2024-01-02 15:14:53 +0100

    PhpStormStubsSourceStubber - do not remove `Override` attribute from older PHP versions

--- src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
+++ src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
@@ -417,6 +417,7 @@ final class PhpStormStubsSourceStubber implements SourceStubber
                     && $className !== 'ReturnTypeWillChange'
                     && $className !== 'AllowDynamicProperties'
                     && $className !== 'SensitiveParameter'
+                    && $className !== 'Override'
                     && ! $this->isSupportedInPhpVersion($classNode)
                 ) {
                     continue;
