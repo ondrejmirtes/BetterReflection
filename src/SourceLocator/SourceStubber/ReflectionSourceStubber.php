b8d1c3aaf28723e7b1008b9c6a70fc08b6e42f68
Ondrej Mirtes <ondrej@mirtes.cz>
2024-05-13 14:28:28 +0200

    ReflectionSourceStubber - Fix new in default parameter value initializer

--- src/SourceLocator/SourceStubber/ReflectionSourceStubber.php
+++ src/SourceLocator/SourceStubber/ReflectionSourceStubber.php
@@ -624,6 +624,11 @@ final class ReflectionSourceStubber implements SourceStubber
                 ));
                 return;
             }
+
+            $parameterNode->setDefault(new Node\Expr\New_(
+                new FullyQualified($className)
+            ));
+            return;
         }
 
         $parameterNode->setDefault($defaultValue);
