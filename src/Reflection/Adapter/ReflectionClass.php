f52ac86cb03a2eaf976c50c5e08f3b1681392091
Jaroslav Hanslík <kukulich@kukulich.cz>
2024-08-03 11:47:21 +0200

    Compatibility with typed constants in PHP 8.4

--- src/Reflection/Adapter/ReflectionClass.php
+++ src/Reflection/Adapter/ReflectionClass.php
@@ -42,6 +42,9 @@ final class ReflectionClass extends CoreReflectionClass
     /** @internal */
     public const SKIP_DESTRUCTOR_COMPATIBILITY = 16;
 
+    /** @internal */
+    public const IS_READONLY_COMPATIBILITY = 65536;
+
     public function __construct(private BetterReflectionClass|BetterReflectionEnum $betterReflectionClass)
     {
         unset($this->name);
@@ -363,7 +366,7 @@ final class ReflectionClass extends CoreReflectionClass
             $this->betterReflectionClass instanceof BetterReflectionEnum
             && (
                 $filter === null
-                || $filter & ReflectionClassConstant::IS_PUBLIC
+                || $filter & ReflectionClassConstant::IS_PUBLIC_COMPATIBILITY
             )
         ) {
             $reflectionConstants += $this->betterReflectionClass->getCases();
