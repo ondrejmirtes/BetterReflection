f2ab8529baec6328c6d00e358139467f80c2ffd8
Ondrej Mirtes <ondrej@mirtes.cz>
2024-08-26 22:46:18 +0200

    Nullability for 8.4

--- src/Reflection/Adapter/ReflectionClass.php
+++ src/Reflection/Adapter/ReflectionClass.php
@@ -502,7 +502,7 @@ final class ReflectionClass extends CoreReflectionClass
         return $reflection->newInstanceWithoutConstructor();
     }
 
-    public function newInstanceArgs(array $args = null): object
+    public function newInstanceArgs(?array $args = null): object
     {
         ClassExistenceChecker::classExists($this->getName(), true);
         $reflection = new CoreReflectionClass($this->getName());
