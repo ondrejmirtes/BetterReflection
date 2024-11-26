6ae68b4363e7f02463f29771981c01b52ea4c7aa
Ondrej Mirtes <ondrej@mirtes.cz>
2024-11-26 20:58:16 +0100

    [GENERATED] Downgraded

--- src/Reflection/ReflectionConstant.php
+++ src/Reflection/ReflectionConstant.php
@@ -29,6 +29,12 @@ use function is_int;
 /** @psalm-immutable */
 class ReflectionConstant implements Reflection
 {
+    private Reflector $reflector;
+    private LocatedSource $locatedSource;
+    /**
+     * @var non-empty-string|null
+     */
+    private $namespace = null;
     /**
      * @var non-empty-string
      * @psalm-allow-private-mutation
@@ -44,7 +50,7 @@ class ReflectionConstant implements Reflection
     private Node\Expr $value;
 
     /** @var non-empty-string|null */
-    private string|null $docComment;
+    private $docComment;
 
     /** @var positive-int */
     private int $startLine;
@@ -58,20 +64,19 @@ class ReflectionConstant implements Reflection
     /** @var positive-int */
     private int $endColumn;
 
-    /** @psalm-allow-private-mutation */
-    private CompiledValue|null $compiledValue = null;
+    /** @psalm-allow-private-mutation
+     * @var \Roave\BetterReflection\NodeCompiler\CompiledValue|null */
+    private $compiledValue = null;
 
-    /** @param non-empty-string|null $namespace */
-    private function __construct(
-        private Reflector $reflector,
-        Node\Stmt\Const_|Node\Expr\FuncCall $node,
-        private LocatedSource $locatedSource,
+    /** @param non-empty-string|null $namespace
+     * @param \PhpParser\Node\Stmt\Const_|\PhpParser\Node\Expr\FuncCall $node */
+    private function __construct(Reflector $reflector, $node, LocatedSource $locatedSource, ?string $namespace = null, ?int $positionInNode = null)
+    {
+        $this->reflector = $reflector;
+        $this->locatedSource = $locatedSource;
         /** @psalm-allow-private-mutation */
-        private string|null $namespace = null,
-        int|null $positionInNode = null,
-    ) {
+        $this->namespace = $namespace;
         $this->setNamesFromNode($node, $positionInNode);
-
         if ($node instanceof Node\Expr\FuncCall) {
             $argumentValueNode = $node->args[1];
             assert($argumentValueNode instanceof Node\Arg);
@@ -80,14 +85,11 @@ class ReflectionConstant implements Reflection
             /** @psalm-suppress PossiblyNullArrayOffset */
             $this->value = $node->consts[$positionInNode]->value;
         }
-
         $this->docComment = GetLastDocComment::forNode($node);
-
         $startLine = $node->getStartLine();
         assert($startLine > 0);
         $endLine = $node->getEndLine();
         assert($endLine > 0);
-
         $this->startLine   = $startLine;
         $this->endLine     = $endLine;
         $this->startColumn = CalculateReflectionColumn::getStartColumn($this->locatedSource->getSource(), $node);
@@ -102,30 +104,19 @@ class ReflectionConstant implements Reflection
      * @param Node\Stmt\Const_|Node\Expr\FuncCall $node      Node has to be processed by the PhpParser\NodeVisitor\NameResolver
      * @param non-empty-string|null               $namespace
      */
-    public static function createFromNode(
-        Reflector $reflector,
-        Node $node,
-        LocatedSource $locatedSource,
-        string|null $namespace = null,
-        int|null $positionInNode = null,
-    ): self {
+    public static function createFromNode(Reflector $reflector, Node $node, LocatedSource $locatedSource, ?string $namespace = null, ?int $positionInNode = null): self
+    {
         if ($node instanceof Node\Stmt\Const_) {
             assert(is_int($positionInNode));
 
             return self::createFromConstKeyword($reflector, $node, $locatedSource, $namespace, $positionInNode);
         }
-
         return self::createFromDefineFunctionCall($reflector, $node, $locatedSource);
     }
 
     /** @param non-empty-string|null $namespace */
-    private static function createFromConstKeyword(
-        Reflector $reflector,
-        Node\Stmt\Const_ $node,
-        LocatedSource $locatedSource,
-        string|null $namespace,
-        int $positionInNode,
-    ): self {
+    private static function createFromConstKeyword(Reflector $reflector, Node\Stmt\Const_ $node, LocatedSource $locatedSource, ?string $namespace, int $positionInNode): self
+    {
         return new self(
             $reflector,
             $node,
@@ -136,13 +127,9 @@ class ReflectionConstant implements Reflection
     }
 
     /** @throws InvalidConstantNode */
-    private static function createFromDefineFunctionCall(
-        Reflector $reflector,
-        Node\Expr\FuncCall $node,
-        LocatedSource $locatedSource,
-    ): self {
+    private static function createFromDefineFunctionCall(Reflector $reflector, Node\Expr\FuncCall $node, LocatedSource $locatedSource): self
+    {
         ConstantNodeChecker::assertValidDefineFunctionCall($node);
-
         return new self(
             $reflector,
             $node,
@@ -178,7 +165,7 @@ class ReflectionConstant implements Reflection
      *
      * @return non-empty-string|null
      */
-    public function getNamespaceName(): string|null
+    public function getNamespaceName(): ?string
     {
         return $this->namespace;
     }
@@ -193,7 +180,7 @@ class ReflectionConstant implements Reflection
     }
 
     /** @return non-empty-string|null */
-    public function getExtensionName(): string|null
+    public function getExtensionName(): ?string
     {
         return $this->locatedSource->getExtensionName();
     }
@@ -228,7 +215,7 @@ class ReflectionConstant implements Reflection
     /**
      * @return mixed
      */
-    public function getValue(): mixed
+    public function getValue()
     {
         if ($this->compiledValue === null) {
             $this->compiledValue = (new CompileNodeToValue())->__invoke(
@@ -241,7 +228,7 @@ class ReflectionConstant implements Reflection
     }
 
     /** @return non-empty-string|null */
-    public function getFileName(): string|null
+    public function getFileName(): ?string
     {
         return $this->locatedSource->getFileName();
     }
@@ -284,7 +271,7 @@ class ReflectionConstant implements Reflection
     }
 
     /** @return non-empty-string|null */
-    public function getDocComment(): string|null
+    public function getDocComment(): ?string
     {
         return $this->docComment;
     }
@@ -295,7 +282,10 @@ class ReflectionConstant implements Reflection
         return ReflectionConstantStringCast::toString($this);
     }
 
-    private function setNamesFromNode(Node\Stmt\Const_|Node\Expr\FuncCall $node, int|null $positionInNode): void
+    /**
+     * @param \PhpParser\Node\Stmt\Const_|\PhpParser\Node\Expr\FuncCall $node
+     */
+    private function setNamesFromNode($node, ?int $positionInNode): void
     {
         if ($node instanceof Node\Expr\FuncCall) {
             $name = $this->getNameFromDefineFunctionCall($node);
--- test/unit/Reflection/Adapter/ReflectionEnumBackedCaseTest.php
+++ test/unit/Reflection/Adapter/ReflectionEnumBackedCaseTest.php
@@ -69,9 +69,10 @@ class ReflectionEnumBackedCaseTest extends TestCase
         ];
     }
 
-    /** @param list<mixed> $args */
+    /** @param list<mixed> $args
+     * @param mixed $returnValue */
     #[DataProvider('methodExpectationProvider')]
-    public function testAdapterMethods(string $methodName, string|null $expectedException, mixed $returnValue, array $args): void
+    public function testAdapterMethods(string $methodName, ?string $expectedException, $returnValue, array $args): void
     {
         $reflectionStub = $this->createMock(BetterReflectionEnumCase::class);
 
@@ -157,7 +158,7 @@ class ReflectionEnumBackedCaseTest extends TestCase
             ->willReturn($betterReflectionAttributes);
 
         $reflectionEnumBackedCaseAdapter = new ReflectionEnumBackedCaseAdapter($betterReflectionEnumCase);
-        $attributes                      = $reflectionEnumBackedCaseAdapter->getAttributes();
+        $attributes                      = method_exists($reflectionEnumBackedCaseAdapter, 'getAttributes') ? $reflectionEnumBackedCaseAdapter->getAttributes() : [];
 
         self::assertCount(2, $attributes);
         self::assertSame('SomeAttribute', $attributes[0]->getName());
@@ -192,7 +193,7 @@ class ReflectionEnumBackedCaseTest extends TestCase
             ->willReturn($betterReflectionAttributes);
 
         $reflectionEnumBackedCaseAdapter = new ReflectionEnumBackedCaseAdapter($betterReflectionEnumCase);
-        $attributes                      = $reflectionEnumBackedCaseAdapter->getAttributes($someAttributeClassName);
+        $attributes                      = method_exists($reflectionEnumBackedCaseAdapter, 'getAttributes') ? $reflectionEnumBackedCaseAdapter->getAttributes($someAttributeClassName) : [];
 
         self::assertCount(1, $attributes);
         self::assertSame($someAttributeClassName, $attributes[0]->getName());
@@ -294,9 +295,9 @@ class ReflectionEnumBackedCaseTest extends TestCase
 
         $reflectionEnumBackedCaseAdapter = new ReflectionEnumBackedCaseAdapter($betterReflectionEnumCase);
 
-        self::assertCount(1, $reflectionEnumBackedCaseAdapter->getAttributes($className, ReflectionAttributeAdapter::IS_INSTANCEOF));
-        self::assertCount(2, $reflectionEnumBackedCaseAdapter->getAttributes($parentClassName, ReflectionAttributeAdapter::IS_INSTANCEOF));
-        self::assertCount(2, $reflectionEnumBackedCaseAdapter->getAttributes($interfaceName, ReflectionAttributeAdapter::IS_INSTANCEOF));
+        self::assertCount(1, method_exists($reflectionEnumBackedCaseAdapter, 'getAttributes') ? $reflectionEnumBackedCaseAdapter->getAttributes($className, ReflectionAttributeAdapter::IS_INSTANCEOF) : []);
+        self::assertCount(2, method_exists($reflectionEnumBackedCaseAdapter, 'getAttributes') ? $reflectionEnumBackedCaseAdapter->getAttributes($parentClassName, ReflectionAttributeAdapter::IS_INSTANCEOF) : []);
+        self::assertCount(2, method_exists($reflectionEnumBackedCaseAdapter, 'getAttributes') ? $reflectionEnumBackedCaseAdapter->getAttributes($interfaceName, ReflectionAttributeAdapter::IS_INSTANCEOF) : []);
     }
 
     public function testGetAttributesThrowsExceptionForInvalidFlags(): void
@@ -305,7 +306,7 @@ class ReflectionEnumBackedCaseTest extends TestCase
         $reflectionEnumBackedCaseAdapter = new ReflectionEnumBackedCaseAdapter($betterReflectionEnumCase);
 
         $this->expectException(Error::class);
-        $reflectionEnumBackedCaseAdapter->getAttributes(null, 123);
+        method_exists($reflectionEnumBackedCaseAdapter, 'getAttributes') ? $reflectionEnumBackedCaseAdapter->getAttributes(null, 123) : [];
     }
 
     public function testIsFinal(): void
