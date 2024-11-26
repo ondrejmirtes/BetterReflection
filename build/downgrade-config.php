a2476f884e02a847d31ae49712102756e62cfc4a
Ondrej Mirtes <ondrej@mirtes.cz>
2024-11-26 20:55:39 +0100

    Update Rector again

--- build/downgrade-config.php
+++ build/downgrade-config.php
@@ -4,12 +4,14 @@ use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
-return static function (RectorConfig $config): void {
-    $config->skip([
+
+return RectorConfig::configure()
+    ->withSkip([
         dirname(__DIR__) . '/test/unit/Fixture',
         dirname(__DIR__) . '/src/Reflection/Adapter/ReflectionEnum*',
         \Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector::class,
         RenameFunctionRector::class,
+    ])
+    ->withSets([
+        DowngradeLevelSetList::DOWN_TO_PHP_74,
     ]);
-    $config->sets([DowngradeLevelSetList::DOWN_TO_PHP_74]);
-};
