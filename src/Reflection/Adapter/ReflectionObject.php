5e3cf20645a70412b55ec6eefdf84b20d94fab3c
Ondrej Mirtes <ondrej@mirtes.cz>
2024-11-27 14:45:32 +0100

    [GENERATED] Downgraded

--- src/Reflection/Adapter/ReflectionObject.php
+++ src/Reflection/Adapter/ReflectionObject.php
@@ -29,8 +29,10 @@ use function strtolower;
 /** @psalm-suppress PropertyNotSetInConstructor */
 final class ReflectionObject extends CoreReflectionObject
 {
-    public function __construct(private BetterReflectionObject $betterReflectionObject)
+    private BetterReflectionObject $betterReflectionObject;
+    public function __construct(BetterReflectionObject $betterReflectionObject)
     {
+        $this->betterReflectionObject = $betterReflectionObject;
         unset($this->name);
     }
 
@@ -111,7 +113,7 @@ final class ReflectionObject extends CoreReflectionObject
         return $this->betterReflectionObject->getDocComment() ?? false;
     }
 
-    public function getConstructor(): ReflectionMethod|null
+    public function getConstructor(): ?\Roave\BetterReflection\Reflection\Adapter\ReflectionMethod
     {
         $constructor = $this->betterReflectionObject->getConstructor();
 
@@ -232,10 +234,10 @@ final class ReflectionObject extends CoreReflectionObject
      *
      * @return array<non-empty-string, mixed>
      */
-    public function getConstants(int|null $filter = null): array
+    public function getConstants(?int $filter = null): array
     {
         return array_map(
-            static fn (BetterReflectionClassConstant $betterConstant): mixed => $betterConstant->getValue(),
+            static fn (BetterReflectionClassConstant $betterConstant) => $betterConstant->getValue(),
             $this->betterReflectionObject->getConstants($filter ?? 0),
         );
     }
@@ -283,7 +285,7 @@ final class ReflectionObject extends CoreReflectionObject
      *
      * @return list<ReflectionClassConstant>
      */
-    public function getReflectionConstants(int|null $filter = null): array
+    public function getReflectionConstants(?int $filter = null): array
     {
         return array_values(array_map(
             static fn (BetterReflectionClassConstant $betterConstant): ReflectionClassConstant => new ReflectionClassConstant($betterConstant),
@@ -605,7 +607,7 @@ final class ReflectionObject extends CoreReflectionObject
      *
      * @return list<ReflectionAttribute|FakeReflectionAttribute>
      */
-    public function getAttributes(string|null $name = null, int $flags = 0): array
+    public function getAttributes(?string $name = null, int $flags = 0): array
     {
         if ($flags !== 0 && $flags !== ReflectionAttribute::IS_INSTANCEOF) {
             throw new ValueError('Argument #2 ($flags) must be a valid attribute filter flag');
@@ -619,7 +621,7 @@ final class ReflectionObject extends CoreReflectionObject
             $attributes = $this->betterReflectionObject->getAttributes();
         }
 
-        return array_map(static fn (BetterReflectionAttribute $betterReflectionAttribute): ReflectionAttribute|FakeReflectionAttribute => ReflectionAttributeFactory::create($betterReflectionAttribute), $attributes);
+        return array_map(static fn (BetterReflectionAttribute $betterReflectionAttribute) => ReflectionAttributeFactory::create($betterReflectionAttribute), $attributes);
     }
 
     public function isEnum(): bool
@@ -627,7 +629,10 @@ final class ReflectionObject extends CoreReflectionObject
         return $this->betterReflectionObject->isEnum();
     }
 
-    public function __get(string $name): mixed
+    /**
+     * @return mixed
+     */
+    public function __get(string $name)
     {
         if ($name === 'name') {
             return $this->betterReflectionObject->getName();
--- src/Reflection/ReflectionMethod.php
+++ src/Reflection/ReflectionMethod.php
@@ -28,6 +28,19 @@ use function strtolower;
 /** @psalm-immutable */
 class ReflectionMethod
 {
+    private Reflector $reflector;
+    private LocatedSource $locatedSource;
+    /**
+     * @var non-empty-string|null
+     */
+    private $namespace;
+    private ReflectionClass $declaringClass;
+    private ReflectionClass $implementingClass;
+    private ReflectionClass $currentClass;
+    /**
+     * @var non-empty-string|null
+     */
+    private $aliasName;
     use ReflectionFunctionAbstract;
 
     /** @var int-mask-of<ReflectionMethodAdapter::IS_*> */
@@ -37,23 +50,20 @@ class ReflectionMethod
      * @param non-empty-string      $name
      * @param non-empty-string|null $aliasName
      * @param non-empty-string|null $namespace
+     * @param MethodNode|\PhpParser\Node\PropertyHook|\PhpParser\Node\Stmt\Function_|\PhpParser\Node\Expr\Closure|\PhpParser\Node\Expr\ArrowFunction $node
      */
-    private function __construct(
-        private Reflector $reflector,
-        MethodNode|Node\PropertyHook|Node\Stmt\Function_|Node\Expr\Closure|Node\Expr\ArrowFunction $node,
-        private LocatedSource $locatedSource,
-        string $name,
-        private string|null $namespace,
-        private ReflectionClass $declaringClass,
-        private ReflectionClass $implementingClass,
-        private ReflectionClass $currentClass,
-        private string|null $aliasName,
-    ) {
+    private function __construct(Reflector $reflector, $node, LocatedSource $locatedSource, string $name, ?string $namespace, ReflectionClass $declaringClass, ReflectionClass $implementingClass, ReflectionClass $currentClass, ?string $aliasName)
+    {
+        $this->reflector = $reflector;
+        $this->locatedSource = $locatedSource;
+        $this->namespace = $namespace;
+        $this->declaringClass = $declaringClass;
+        $this->implementingClass = $implementingClass;
+        $this->currentClass = $currentClass;
+        $this->aliasName = $aliasName;
         assert($node instanceof MethodNode || $node instanceof Node\PropertyHook);
-
         $this->name      = $name;
         $this->modifiers = $node instanceof MethodNode ? $this->computeModifiers($node) : 0;
-
         $this->fillFromNode($node);
     }
 
@@ -63,18 +73,10 @@ class ReflectionMethod
      * @param non-empty-string      $name
      * @param non-empty-string|null $aliasName
      * @param non-empty-string|null $namespace
+     * @param MethodNode|\PhpParser\Node\PropertyHook $node
      */
-    public static function createFromNode(
-        Reflector $reflector,
-        MethodNode|Node\PropertyHook $node,
-        LocatedSource $locatedSource,
-        string $name,
-        string|null $namespace,
-        ReflectionClass $declaringClass,
-        ReflectionClass $implementingClass,
-        ReflectionClass $currentClass,
-        string|null $aliasName = null,
-    ): self {
+    public static function createFromNode(Reflector $reflector, $node, LocatedSource $locatedSource, string $name, ?string $namespace, ReflectionClass $declaringClass, ReflectionClass $implementingClass, ReflectionClass $currentClass, ?string $aliasName = null): self
+    {
         return new self(
             $reflector,
             $node,
@@ -114,7 +116,7 @@ class ReflectionMethod
      * @param non-empty-string|null                      $aliasName
      * @param int-mask-of<ReflectionMethodAdapter::IS_*> $modifiers
      */
-    public function withImplementingClass(ReflectionClass $implementingClass, string|null $aliasName, int $modifiers): self
+    public function withImplementingClass(ReflectionClass $implementingClass, ?string $aliasName, int $modifiers): self
     {
         $clone = clone $this;
 
@@ -161,7 +163,7 @@ class ReflectionMethod
     }
 
     /** @return non-empty-string|null */
-    public function getAliasName(): string|null
+    public function getAliasName(): ?string
     {
         return $this->aliasName;
     }
@@ -187,7 +189,7 @@ class ReflectionMethod
         $currentClass = $currentClass->getParentClass();
 
         if ($currentClass !== null) {
-            $prototype = $currentClass->getMethod($this->getName())?->findPrototype();
+            $prototype = ($nullsafeVariable1 = $currentClass->getMethod($this->getName())) ? $nullsafeVariable1->findPrototype() : null;
 
             if (
                 $prototype !== null
@@ -207,7 +209,7 @@ class ReflectionMethod
         ));
     }
 
-    private function findPrototype(): self|null
+    private function findPrototype(): ?self
     {
         if ($this->isAbstract()) {
             return $this;
@@ -219,7 +221,7 @@ class ReflectionMethod
 
         try {
             return $this->getPrototype();
-        } catch (Exception\MethodPrototypeNotFound) {
+        } catch (Exception\MethodPrototypeNotFound $exception) {
             return $this;
         }
     }
@@ -258,7 +260,7 @@ class ReflectionMethod
         return false;
     }
 
-    public function getNamespaceName(): string|null
+    public function getNamespaceName(): ?string
     {
         return null;
     }
@@ -373,27 +375,29 @@ class ReflectionMethod
      * @throws NoObjectProvided
      * @throws ObjectNotInstanceOfClass
      */
-    public function getClosure(object|null $object = null): Closure
+    public function getClosure(?object $object = null): Closure
     {
         $declaringClassName = $this->getDeclaringClass()->getName();
 
         if ($this->isStatic()) {
             $this->assertClassExist($declaringClassName);
 
-            return fn (mixed ...$args): mixed => $this->callStaticMethod($args);
+            return fn (...$args) => $this->callStaticMethod($args);
         }
 
         $instance = $this->assertObject($object);
 
-        return fn (mixed ...$args): mixed => $this->callObjectMethod($instance, $args);
+        return fn (...$args) => $this->callObjectMethod($instance, $args);
     }
 
     /**
      * @throws ClassDoesNotExist
      * @throws NoObjectProvided
      * @throws ObjectNotInstanceOfClass
+     * @param mixed ...$args
+     * @return mixed
      */
-    public function invoke(object|null $object = null, mixed ...$args): mixed
+    public function invoke(?object $object = null, ...$args)
     {
         return $this->invokeArgs($object, $args);
     }
@@ -404,8 +408,9 @@ class ReflectionMethod
      * @throws ClassDoesNotExist
      * @throws NoObjectProvided
      * @throws ObjectNotInstanceOfClass
+     * @return mixed
      */
-    public function invokeArgs(object|null $object = null, array $args = []): mixed
+    public function invokeArgs(?object $object = null, array $args = [])
     {
         $implementingClassName = $this->getImplementingClass()->getName();
 
@@ -418,13 +423,14 @@ class ReflectionMethod
         return $this->callObjectMethod($this->assertObject($object), $args);
     }
 
-    /** @param array<mixed> $args */
-    private function callStaticMethod(array $args): mixed
+    /** @param array<mixed> $args
+     * @return mixed */
+    private function callStaticMethod(array $args)
     {
         $implementingClassName = $this->getImplementingClass()->getName();
 
         /** @psalm-suppress InvalidStringClass */
-        $closure = Closure::bind(fn (string $implementingClassName, string $_methodName, array $methodArgs): mixed => $implementingClassName::{$_methodName}(...$methodArgs), null, $implementingClassName);
+        $closure = Closure::bind(fn (string $implementingClassName, string $_methodName, array $methodArgs) => $implementingClassName::{$_methodName}(...$methodArgs), null, $implementingClassName);
 
         /** @phpstan-ignore function.alreadyNarrowedType, instanceof.alwaysTrue */
         assert($closure instanceof Closure);
@@ -432,11 +438,12 @@ class ReflectionMethod
         return $closure->__invoke($implementingClassName, $this->getName(), $args);
     }
 
-    /** @param array<mixed> $args */
-    private function callObjectMethod(object $object, array $args): mixed
+    /** @param array<mixed> $args
+     * @return mixed */
+    private function callObjectMethod(object $object, array $args)
     {
         /** @psalm-suppress MixedMethodCall */
-        $closure = Closure::bind(fn (object $object, string $methodName, array $methodArgs): mixed => $object->{$methodName}(...$methodArgs), $object, $this->getImplementingClass()->getName());
+        $closure = Closure::bind(fn (object $object, string $methodName, array $methodArgs) => $object->{$methodName}(...$methodArgs), $object, $this->getImplementingClass()->getName());
 
         /** @phpstan-ignore function.alreadyNarrowedType, instanceof.alwaysTrue */
         assert($closure instanceof Closure);
@@ -456,7 +463,7 @@ class ReflectionMethod
      * @throws NoObjectProvided
      * @throws ObjectNotInstanceOfClass
      */
-    private function assertObject(object|null $object): object
+    private function assertObject(?object $object): object
     {
         if ($object === null) {
             throw NoObjectProvided::create();
@@ -464,7 +471,7 @@ class ReflectionMethod
 
         $implementingClassName = $this->getImplementingClass()->getName();
 
-        if ($object::class !== $implementingClassName) {
+        if (get_class($object) !== $implementingClassName) {
             throw ObjectNotInstanceOfClass::fromClassName($implementingClassName);
         }
 
