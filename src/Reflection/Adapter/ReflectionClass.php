bb215d72790fb3b13669a165470ccb2a9c64132b
Ondrej Mirtes <ondrej@mirtes.cz>
2024-11-28 21:31:05 +0100

    Make easier downgrading for methods returning `never`

--- src/Reflection/Adapter/ReflectionClass.php
+++ src/Reflection/Adapter/ReflectionClass.php
@@ -577,13 +577,13 @@ final class ReflectionClass extends CoreReflectionClass
     }
 
     /** @param int-mask-of<self::SKIP_*> $options */
-    public function resetAsLazyGhost(object $object, callable $initializer, int $options = 0): never
+    public function resetAsLazyGhost(object $object, callable $initializer, int $options = 0): void
     {
         throw Exception\NotImplementedBecauseItTriggersAutoloading::create();
     }
 
     /** @param int-mask-of<self::SKIP_*> $options */
-    public function resetAsLazyProxy(object $object, callable $factory, int $options = 0): never
+    public function resetAsLazyProxy(object $object, callable $factory, int $options = 0): void
     {
         throw Exception\NotImplementedBecauseItTriggersAutoloading::create();
     }
