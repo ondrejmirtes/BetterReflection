d251bef9aa158009ae881aa985db12e8ac955d2c
Markus Staab <maggus.staab@googlemail.com>
2025-08-29 13:18:17 +0200

    Get rid of array_combine in getTraits()

--- src/Reflection/Adapter/ReflectionClass.php
+++ src/Reflection/Adapter/ReflectionClass.php
@@ -434,14 +434,11 @@ final class ReflectionClass extends CoreReflectionClass
     {
         $traits = $this->betterReflectionClass->getTraits();
 
-        /** @var list<trait-string> $traitNames */
-        $traitNames = array_map(static fn (BetterReflectionClass $trait): string => $trait->getName(), $traits);
-
-        /** @psalm-suppress ImpureFunctionCall */
-        return array_combine(
-            $traitNames,
-            array_map(static fn (BetterReflectionClass $trait): self => new self($trait), $traits),
-        );
+        $traitsByName = [];
+        foreach($traits as $trait) {
+            $traitsByName[$trait->getName()] = new self($trait);
+        }
+        return $traitsByName;
     }
 
     /**
