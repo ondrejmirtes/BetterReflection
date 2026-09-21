46750159028f6ddfdd27039027017fb37751353b
Ondrej Mirtes <ondrej@mirtes.cz>
2026-09-21 14:09:04 +0200

    [GENERATED] Renamed

--- src/SourceLocator/Type/MemoizingSourceLocator.php
+++ src/SourceLocator/Type/MemoizingSourceLocator.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Roave\BetterReflection\SourceLocator\Type;
+namespace PHPStan\BetterReflection\SourceLocator\Type;
 
-use Roave\BetterReflection\Identifier\Identifier;
-use Roave\BetterReflection\Identifier\IdentifierType;
-use Roave\BetterReflection\Reflection\Reflection;
-use Roave\BetterReflection\Reflector\Reflector;
+use PHPStan\BetterReflection\Identifier\Identifier;
+use PHPStan\BetterReflection\Identifier\IdentifierType;
+use PHPStan\BetterReflection\Reflection\Reflection;
+use PHPStan\BetterReflection\Reflector\Reflector;
 
 use function array_key_exists;
 use function array_key_first;
@@ -35,7 +35,7 @@ final class MemoizingSourceLocator implements SourceLocator
         $this->maxCachedEntries = $maxCachedEntries;
     }
 
-    public function locateIdentifier(Reflector $reflector, Identifier $identifier): ?\Roave\BetterReflection\Reflection\Reflection
+    public function locateIdentifier(Reflector $reflector, Identifier $identifier): ?\PHPStan\BetterReflection\Reflection\Reflection
     {
         $cacheKey = sprintf('%s_%s', $this->reflectorCacheKey($reflector), $this->identifierToCacheKey($identifier));
 
